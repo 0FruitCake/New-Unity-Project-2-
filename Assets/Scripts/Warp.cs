24d0c086c3d5f248c2d0ef17f9dde0422683b062
karl brian lim <kbd.lim@gmail.com>
2018-07-19 21:23:47 +0800

    Initial commit

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform warpTarget;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.transform.position = warpTarget.position;
+        
+    }
+}
