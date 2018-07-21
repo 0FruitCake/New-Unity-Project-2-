591061bd3cf0f7a0563e98f0557f7e9a24aeee77
FruitCake <kbd.lim@gmail.com>
2018-07-21 17:07:23 +0800

    maps

---
+++ Maps/assets/castlefloors.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castlefloors" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../assets/LPC Base Assets/tiles/castlefloors.png" width="320" height="320"/>
+ <tile id="72">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0" width="32" height="31.6364"/>
+  </objectgroup>
+ </tile>
+</tileset>
