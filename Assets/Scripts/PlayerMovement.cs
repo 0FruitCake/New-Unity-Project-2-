188f150b0830ed9a04d880367ca3bfe273b555b2
ppvconadac <shagitee@gmail.com>
2018-07-25 21:01:33 +0800

    animator and playermovement script changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,41 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int mspeed;
-    Rigidbody2D rbody;
-    Animator anim;
+    public float moveSpeed;
+    private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
 
     // Use this for initialization
     void Start()
     {
-        rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerMoving = false;
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+
     }
 
 
-    void FixedUpdate()
-    {
-        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-        if (movement_vector != Vector2.zero)
-        {
-            anim.SetBool("iswalking", true);
-            anim.SetFloat("input_x", movement_vector.x);
-            anim.SetFloat("input_y", movement_vector.y);
-        }
-        else
-        {
-            anim.SetBool("iswalking", false);
-        }
-
-        transform.Translate(movement_vector * Time.deltaTime * mspeed);
-        
-    }
+  
 }
 
 
