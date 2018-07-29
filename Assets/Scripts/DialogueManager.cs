330c3ccc203d0528b7bfa11694a0f7ae6a30411f
FruitCake <kbd.lim@gmail.com>
2018-07-29 20:09:58 +0800

    switch image

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour {
 
     public GameObject dBox;
+    public GameObject image;
     public Text dText;
     public bool dialogActive;
     public string[] dialogLines;
+    public Sprite boy;
+    public Sprite girl;
+    public Image curimage;
     public int currentLine;
+    public int charactime;
 
 	// Use this for initialization
 	void Start () {
-		
+        	
 	}
 	
 	// Update is called once per frame
@@ -23,14 +28,25 @@ public class DialogueManager : MonoBehaviour {
             // dBox.SetActive(false);
             // dialogActive = false;
             currentLine++;
+            charactime++;
         }
         if (currentLine >= dialogLines.Length) {
             dBox.SetActive(false);
              dialogActive = false;
 
             currentLine = 0;
+            charactime = 0;
         }
+      
         dText.text = dialogLines[currentLine];
+        if (charactime == 2)
+        {
+            image.GetComponent<Image>().sprite = girl;
+        }
+        else
+        {
+            image.GetComponent<Image>().sprite = boy;
+        }
     }
 
     public void ShowBox(string dialogue)
