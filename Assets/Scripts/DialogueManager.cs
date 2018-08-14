14b142c469ecbd037c7c9b05eec00f06bd2412a8
FruitCake <kbd.lim@gmail.com>
2018-08-14 22:52:06 +0800

    choices(legit)

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -11,6 +11,7 @@ public class DialogueManager : MonoBehaviour
     public GameObject btn1;
     public GameObject btn2;
     public Text dText;
+    public Text dtext2;
     public bool dialogActive;
     public string[] dialogLines;
     public Sprite[] images;
@@ -20,8 +21,14 @@ public class DialogueManager : MonoBehaviour
     public string name2;
     public LoadNewArea lna;
     public TextAsset textFile;
+    public TextAsset reply;
     public string[] textLines;
+    public string[] textLines2;
     public int i = 0;
+    public int choice = 0;
+    public int j;
+    public int k;
+
 
 
 
@@ -38,44 +45,71 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (i < textLines.Length - 1)       //put to dialog
-        {
-            int pic = int.Parse(textLines[i + 2]);
-            dText.text = textLines[i] + ": " + textLines[i + 1];
-            image.GetComponent<Image>().sprite = images[pic];
-
-        }
         if (dialogActive && Input.GetKeyDown(KeyCode.Space))
         {
             currentLine++;
             i += 3;
         }
 
-
-        if (currentLine > textLines.Length / 3 - 1)  // pag wla nay lines
+        if (choice == 0)
         {
-            dBox.SetActive(false);
-            dialogActive = false;
-
-            if (textLines.Length % 3 != 0) // button loop, kay 7 ang naa sa array... nagdagdag kog isa ka line para maidentify na choices sya
+            if (i < textLines.Length - 1)       //put to dialog
             {
+                int pic = int.Parse(textLines[i + 2]);
+                dText.text = textLines[i] + ": " + textLines[i + 1];
+                image.GetComponent<Image>().sprite = images[pic];
 
-                btn1.SetActive(true);
-                btn2.SetActive(true);
-            }
-            else
-            {
-
-                Debug.Log(name);
-                if (name2 == "Assistant")
-                {
-                    lna.loadFromDialogue(name2);
-                }
             }
 
 
         }
+        else if (choice == 1) {
+            if (reply != null)    //file readet
+            {
+                textLines2 = (reply.text.Split('\n', ';'));
+            }
+            else {
+                dialogActive = false;
+                dBox.SetActive(false);
+                choice = 0;
+            }
 
+            if (i < textLines2.Length - 1)       //put to dialog
+            {
+                int pic = int.Parse(textLines2[i + 2]);
+                dText.text = textLines2[i] + ": " + textLines2[i + 1];
+                image.GetComponent<Image>().sprite = images[pic];
+
+            }
+      
+            if (currentLine > textLines2.Length / 3 - 1)  // pag wla nay lines
+            {
+                dialogActive = false;
+                dBox.SetActive(false);
+                choice = 0;
+                i = 0;
+                currentLine = 0;
+            }
+           }
+
+        if (currentLine > textLines.Length / 3 - 1)  // pag wla nay lines
+        {
+            if (textLines.Length % 3 != 0) // button loop, kay 7 ang naa sa array... nagdagdag kog isa ka line para maidentify na choices sya
+            {
+                btn1.SetActive(true);
+                btn2.SetActive(true);
+
+            }
+            else
+            {
+
+                dialogActive = false;
+                dBox.SetActive(false);
+                currentLine = 0;
+                i = 0;
+            }
+
+        }
     }
 
     public void showDialogue(string name1)
@@ -83,5 +117,22 @@ public class DialogueManager : MonoBehaviour
         dialogActive = true;
         dBox.SetActive(true);
         name2 = name1;
+    }
+
+    public void choosebtn() {
+
+        Debug.Log(name);
+        if (name2 == "Assistant")
+        {
+            lna.loadFromDialogue(name2);
+        }
+
+    }
+    public void choosebtn1() {
+        choice = 1;
+        btn1.SetActive(false);
+        btn2.SetActive(false);
+        i = 0;
+        currentLine = 0;
     }
 }
